610253de5e7b5ccb455902bd882a8850c61f5e50
zorlack <daveseltzer@gmail.com>
2013-11-05 19:50:12 -0500

    First import of Arduino code for DMX Projection Manager

---
+++ DMXKeyboardDowser.ino
@@ -0,0 +1,114 @@
+// - - - - -
+// DmxSerial - A hardware supported interface to DMX.
+// DmxSerialRecv.ino: Sample DMX application for retrieving 3 DMX values:
+// address 1 (red) -> PWM Port 9
+// address 2 (green) -> PWM Port 6
+// address 3 (blue) -> PWM Port 5
+// 
+// Copyright (c) 2011 by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// 
+// Documentation and samples are available at http://www.mathertel.de/Arduino
+// 25.07.2011 creation of the DmxSerial library.
+// 10.09.2011 fully control the serial hardware register
+//            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
+// 01.12.2011 include file and extension changed to work with the Arduino 1.0 environment
+// 28.12.2011 changed to channels 1..3 (RGB) for compatibility with the DmxSerialSend sample.
+// 10.05.2012 added some lines to loop to show how to fall back to a default color when no data was received since some time.
+// - - - - -
+
+#include "DMXSerial.h"
+#include <Servo.h> 
+// Constants for demo program
+
+//Define DMX Addresses
+int dmxServoAddress = 178;
+int dmxKeyboardAddress = 179;
+
+//Define variables for Servo
+int servoPWMPin = 9;
+Servo dowserServo;
+float servoStartPosition = 0.0;
+float servoEndPosition = 180.0;
+int dmxLastServoValue = -128;
+
+//Define thresholds for keyboard data
+char dmxValueLeftKeyCode = KEY_LEFT_ARROW;
+int dmxValueLeftMin = 32;
+int dmxValueLeftMax = 96;
+char dmxValueRightKeyCode = KEY_RIGHT_ARROW;
+int dmxValueRightMin = 160;
+int dmxValueRightMax = 192;
+int dmxLastKeyboardValue = 0;
+int keyPressDuration = 20; //ms
+
+
+
+
+void setup () {
+  //initiailize DMX Receiver
+  DMXSerial.init(DMXReceiver);
+
+  //connect servo object to the appropriate pin
+  dowserServo.attach(servoPWMPin);
+  
+  //initialize keyboard emulation
+  Keyboard.begin();
+  
+  //write some default values to the DMX Serial object
+  DMXSerial.write(dmxServoAddress, 0);
+  DMXSerial.write(dmxKeyboardAddress, 0);
+  
+  //initialize debug output
+  Serial.begin(9600);
+  Serial.println("System Initialized!");
+}
+
+
+void loop() {
+  // Calculate how long no data backet was received
+  unsigned long lastPacket = DMXSerial.noDataSince();
+    
+  int dmxServoValue = DMXSerial.read(dmxServoAddress);
+  int dmxKeyboardValue = DMXSerial.read(dmxKeyboardAddress);
+  
+  //write the appropriate value to the servo:
+  if(dmxServoValue != dmxLastServoValue) 
+  {    
+    float servoValue = (float)dmxServoValue * ((servoEndPosition-servoStartPosition)/255.0);
+    Serial.print("Changing Servo to: ");
+    Serial.print(servoValue);
+    Serial.println(" degrees");
+    
+    //write the values
+    dowserServo.write(servoValue);
+    dmxLastServoValue = dmxServoValue;
+  }
+  
+  if(dmxKeyboardValue != dmxLastKeyboardValue) 
+  {
+    //We've detected a change in keyboard value!
+    //lets write out the appropriate value!
+    if( dmxKeyboardValue  >= dmxValueLeftMin && dmxKeyboardValue < dmxValueLeftMax)
+    {
+      Serial.println("Writing Left Arrow Key");
+      
+      //write the keypress
+      Keyboard.press(dmxValueLeftKeyCode);
+      delay(keyPressDuration);
+      Keyboard.release(dmxValueLeftKeyCode);
+    }
+    if( dmxKeyboardValue  >= dmxValueRightMin && dmxKeyboardValue < dmxValueRightMax)
+    {
+      Serial.println("Writing Right Arrow Key");
+      Keyboard.press(dmxValueRightKeyCode);
+      delay(20);
+      Keyboard.release(dmxValueRightKeyCode);
+    }
+    
+    //update the value of dmxLast.
+    dmxLastKeyboardValue = dmxKeyboardValue;    
+  }
+}
+
+// End.
