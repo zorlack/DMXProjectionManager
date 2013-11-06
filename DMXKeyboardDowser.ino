03a75398d9247ff34824b5b4d12f6aa9e652df3a
zorlack <daveseltzer@gmail.com>
2013-11-05 19:52:55 -0500

    Update DMXKeyboardDowser.ino

--- DMXKeyboardDowser.ino
+++ DMXKeyboardDowser.ino
@@ -1,21 +1,14 @@
-// - - - - -
-// DmxSerial - A hardware supported interface to DMX.
-// DmxSerialRecv.ino: Sample DMX application for retrieving 3 DMX values:
-// address 1 (red) -> PWM Port 9
-// address 2 (green) -> PWM Port 6
-// address 3 (blue) -> PWM Port 5
-// 
+// Code for a Arduino-based DMX Receiver Device which can
+// operate a servo to douse a projector, and also send keyboard
+// instructions to a computer running a slide show.
+//
+// initial version hard-coded to DMX address and to key-send 
+// thresholds.
+//
+// Based on an example by:
 // Copyright (c) 2011 by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-// 
-// Documentation and samples are available at http://www.mathertel.de/Arduino
-// 25.07.2011 creation of the DmxSerial library.
-// 10.09.2011 fully control the serial hardware register
-//            without using the Arduino Serial (HardwareSerial) class to avoid ISR implementation conflicts.
-// 01.12.2011 include file and extension changed to work with the Arduino 1.0 environment
-// 28.12.2011 changed to channels 1..3 (RGB) for compatibility with the DmxSerialSend sample.
-// 10.05.2012 added some lines to loop to show how to fall back to a default color when no data was received since some time.
-// - - - - -
+
 
 #include "DMXSerial.h"
 #include <Servo.h> 
